39bfeff1b0489d6e0f5f2122be99a9908f95d822
Pekar Andrej <andrej.pekar@everlution.sk>
2016-01-06 20:38:04 +0100

    improve scripts

--- Diagram.SRC/Diagram/Src/Model/Nodes.cs
+++ Diagram.SRC/Diagram/Src/Model/Nodes.cs
@@ -27,7 +27,7 @@ namespace Diagram
 
         public void OrderByPositionY()
         {
-            this.Sort((x, y) => x.position.y < y.position.y);
+            this.Sort((x, y) => y.position.y.CompareTo(x.position.y));
         }
     }
 }
