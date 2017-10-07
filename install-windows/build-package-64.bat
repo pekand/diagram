cb475503f72bfb1db49568ceb4af90e85e9090b1
Pekar Andrej <pekand@gmail.com>
2017-10-07 13:17:21 +0200

    remove scripts plugin from installation folder

--- install-windows/build-package-64.bat
+++ install-windows/build-package-64.bat
@@ -2,16 +2,8 @@ rmdir /S /Q .\files
 mkdir .\files
 
 copy ..\Diagram.SRC\Diagram\diagram.ico .\files\
-copy ..\Diagram.SRC\Diagram\bin\x64\Release\Diagram.exe .\files\
-copy ..\Diagram.SRC\Diagram\bin\x64\Release\IronPython.dll .\files\
-copy ..\Diagram.SRC\Diagram\bin\x64\Release\IronPython.Modules.dll .\files\
-copy ..\Diagram.SRC\Diagram\bin\x64\Release\IronPython.SQLite.dll .\files\
-copy ..\Diagram.SRC\Diagram\bin\x64\Release\IronPython.Wpf.dll .\files\
-copy ..\Diagram.SRC\Diagram\bin\x64\Release\Microsoft.Dynamic.dll .\files\
-copy ..\Diagram.SRC\Diagram\bin\x64\Release\Microsoft.Scripting.AspNet.dll .\files\
-copy ..\Diagram.SRC\Diagram\bin\x64\Release\Microsoft.Scripting.dll .\files\
-copy ..\Diagram.SRC\Diagram\bin\x64\Release\Microsoft.Scripting.Metadata.dll .\files\
-copy ..\Diagram.SRC\Diagram\bin\x64\Release\NCalc.dll .\files\
-copy ..\Diagram.SRC\Diagram\bin\x64\Release\Newtonsoft.Json.dll .\files\
+copy ..\Diagram.SRC\Diagram\bin\Release\Diagram.exe .\files\
+copy ..\Diagram.SRC\Diagram\bin\Release\NCalc.dll .\files\
+copy ..\Diagram.SRC\Diagram\bin\Release\Newtonsoft.Json.dll .\files\
 
 pause
