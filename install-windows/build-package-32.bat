e67201c1007c992f1105d9888e4904b881ff6831
Pekar Andrej <pekand@gmail.com>
2017-10-07 14:21:58 +0200

    cleaning project

--- install-windows/build-package-32.bat
+++ install-windows/build-package-32.bat
@@ -2,16 +2,8 @@ rmdir /S /Q .\files
 mkdir .\files
 
 copy ..\Diagram.SRC\Diagram\diagram.ico .\files\
-copy ..\Diagram.SRC\Diagram\bin\Release\Diagram.exe .\files\
-copy ..\Diagram.SRC\Diagram\bin\Release\IronPython.dll .\files\
-copy ..\Diagram.SRC\Diagram\bin\Release\IronPython.Modules.dll .\files\
-copy ..\Diagram.SRC\Diagram\bin\Release\IronPython.SQLite.dll .\files\
-copy ..\Diagram.SRC\Diagram\bin\Release\IronPython.Wpf.dll .\files\
-copy ..\Diagram.SRC\Diagram\bin\Release\Microsoft.Dynamic.dll .\files\
-copy ..\Diagram.SRC\Diagram\bin\Release\Microsoft.Scripting.AspNet.dll .\files\
-copy ..\Diagram.SRC\Diagram\bin\Release\Microsoft.Scripting.dll .\files\
-copy ..\Diagram.SRC\Diagram\bin\Release\Microsoft.Scripting.Metadata.dll .\files\
-copy ..\Diagram.SRC\Diagram\bin\Release\NCalc.dll .\files\
-copy ..\Diagram.SRC\Diagram\bin\Release\Newtonsoft.Json.dll .\files\
+copy ..\Diagram.SRC\Diagram\bin\Release\x86\Diagram.exe .\files\
+copy ..\Diagram.SRC\Diagram\bin\Release\x86\NCalc.dll .\files\
+copy ..\Diagram.SRC\Diagram\bin\Release\x86\Newtonsoft.Json.dll .\files\
 
 pause
--- install-windows/build-package-64.bat
+++ install-windows/build-package-64.bat
@@ -2,8 +2,8 @@ rmdir /S /Q .\files
 mkdir .\files
 
 copy ..\Diagram.SRC\Diagram\diagram.ico .\files\
-copy ..\Diagram.SRC\Diagram\bin\Release\Diagram.exe .\files\
-copy ..\Diagram.SRC\Diagram\bin\Release\NCalc.dll .\files\
-copy ..\Diagram.SRC\Diagram\bin\Release\Newtonsoft.Json.dll .\files\
+copy ..\Diagram.SRC\Diagram\bin\Release\x64\Diagram.exe .\files\
+copy ..\Diagram.SRC\Diagram\bin\Release\x64\NCalc.dll .\files\
+copy ..\Diagram.SRC\Diagram\bin\Release\x64\Newtonsoft.Json.dll .\files\
 
 pause
