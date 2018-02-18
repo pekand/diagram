d07c8d3fef47a6a22c453f39ba3ccead87af9323
Pekar Andrej <pekand@gmail.com>
2018-02-18 22:52:54 +0100

    copy plugins to windows install directory

--- install-windows/build-package-64.bat
+++ install-windows/build-package-64.bat
@@ -1,7 +1,10 @@
 rmdir /S /Q .\files
 mkdir .\files
+mkdir .\plugins
 
 copy ..\Diagram.SRC\Diagram\diagram.ico .\files\
 copy ..\Diagram.SRC\Diagram\bin\Release\x64\Diagram.exe .\files\
+
+xcopy /s ..\Diagram.SRC\Diagram\bin\Release\x64\plugins\*.* .\plugins\
 
 pause
